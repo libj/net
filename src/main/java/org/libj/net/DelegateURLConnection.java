f32398bf80d34e6f4ae76d00c3a666826805a68c
Seva Safris <seva@safris.org>
2019-12-15 13:33:08 +0700

    Return 0 for hashCode(null)

--- src/main/java/org/libj/net/DelegateURLConnection.java
+++ src/main/java/org/libj/net/DelegateURLConnection.java
@@ -278,7 +278,7 @@ public abstract class DelegateURLConnection extends URLConnection {
 
   @Override
   public int hashCode() {
-    return target == null ? 733 : target.hashCode();
+    return target == null ? 0 : target.hashCode();
   }
 
   @Override
