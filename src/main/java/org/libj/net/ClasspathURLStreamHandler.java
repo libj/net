8b8c8baa28605e3558714afc30a49a6296937a66
Seva Safris <seva@safris.org>
2020-11-14 21:29:59 +0700

    Add ClasspathURLStreamHandler, #3

---
+++ src/main/java/org/libj/net/ClasspathURLStreamHandler.java
@@ -0,0 +1,66 @@
+/* Copyright (c) 2020 LibJ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.libj.net;
+
+import java.net.URL;
+import java.net.URLStreamHandler;
+import java.util.Objects;
+
+import org.libj.net.classpath.Handler;
+
+public abstract class ClasspathURLStreamHandler extends URLStreamHandler {
+  static {
+    URLStreamHandlers.register(Handler.class, Handler.Factory.class);
+  }
+
+  protected static boolean loadMe = true;
+
+  /**
+   * Returns a "classpath" protocol {@link URL} for the specified resource path.
+   *
+   * @param resourcePath The resource path.
+   * @return A "classpath" protocol {@link URL} for the specified resource path.
+   * @throws NullPointerException If {@code resourcePath} is null.
+   */
+  public static URL createURL(final String resourcePath) {
+    return URLs.create("classpath:" + Objects.requireNonNull(resourcePath));
+  }
+
+  /**
+   * Returns the first matching resource {@link URL} for the provided classpath
+   * {@link URL}.
+   * <p>
+   * <b>Note:</b> This method only supports URLs with {@code "classpath"}
+   * protocol, and a {@code null} or empty host.
+   *
+   * @param url The {@link URL}.
+   * @return The data for the provided {@link URL}.
+   * @throws IllegalArgumentException If the provided {@link URL} specifies a
+   *           protocol that is not {@code "classpath"}, or a host that is not
+   *           {@code null} or empty.
+   * @throws NullPointerException If the provided {@link URL} is null.
+   */
+  public static URL getResource(final URL url) {
+    if (!"classpath".equals(url.getProtocol()))
+      throw new IllegalArgumentException("Illegal protocol: " + url.getProtocol());
+
+    if (url.getHost() != null && url.getHost().length() > 0)
+      throw new IllegalArgumentException("Illegal host: " + url.getHost());
+
+    return Thread.currentThread().getContextClassLoader().getResource(url.getPath());
+  }
+}
---
+++ src/main/java/org/libj/net/classpath/Handler.java
@@ -0,0 +1,69 @@
+/* Copyright (c) 2020 LibJ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of The MIT License (MIT) along with this
+ * program. If not, see <http://opensource.org/licenses/MIT/>.
+ */
+
+package org.libj.net.classpath;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+
+import org.libj.net.ClasspathURLStreamHandler;
+
+/**
+ * A {@link URLStreamHandler} that implements the "classpath" protocol. This
+ * class can be used to create {@code classpath:/...} URLs that are resolvable
+ * as {@link ClassLoader} resources.
+ */
+public class Handler extends ClasspathURLStreamHandler {
+  public static class Factory implements URLStreamHandlerFactory { // FIXME: jdk9+ URLStreamHandlerProvider
+    static {
+      // Force Handler class to be loaded
+      if (Handler.loadMe);
+    }
+
+    private static Handler handler;
+
+    @Override
+    public Handler createURLStreamHandler(final String protocol) {
+      return !"classpath".equals(protocol) ? null : handler == null ? handler = new Handler() : handler;
+    }
+  }
+
+  /**
+   * @throws MalformedURLException If the provided {@link URL} specifies a
+   *           protocol that is not {@code "classpath"}.
+   * @throws FileNotFoundException If no resource exists at the provided
+   *           {@link URL}.
+   * @throws IOException If an I/O error occurs while opening the connection.
+   * @throws NullPointerException If the provided {@link URL} is null.
+   */
+  @Override
+  protected URLConnection openConnection(final URL url) throws IOException {
+    if (!"classpath".equals(url.getProtocol()))
+      throw new MalformedURLException("Unsupported protocol: " + url.getProtocol());
+
+    final String resourcePath = url.toString().substring(12);
+    final URL resourceUrl = Thread.currentThread().getContextClassLoader().getResource(resourcePath);
+    if (resourceUrl == null)
+      throw new FileNotFoundException(resourcePath);
+
+    return resourceUrl.openConnection();
+  }
+}
